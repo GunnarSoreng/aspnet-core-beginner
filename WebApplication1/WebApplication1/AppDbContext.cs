3736560192d25b7fcdd177f1e7d1aada1bd2c87f
Gunnar Søreng <gunnar.soreng@nhn.no>
2017-12-01 16:26:57 +0100

    First commit

---
+++ WebApplication1/WebApplication1/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1
+{
+    public class AppDbContext : DbContext
+    {
+        // Constructor
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
